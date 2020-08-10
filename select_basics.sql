1060b45cc88097e385d65618b6974c952c0f927f
Vitalie Melnic <vmwhoami@gmail.com>
2020-08-10 22:55:32 +0300

    Add world sql basics

--- select_basics.sql
+++ select_basics.sql
@@ -19,3 +19,35 @@ WHERE name IN ('Sweden', 'Norway','Denmark');
 SELECT name, area
 FROM world
 WHERE area BETWEEN 200000 AND 250000
+
+-- SELECT from WORLD Tutorial
+
+-- 4.How to use WHERE to filter records. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+
+SELECT name
+FROM world
+WHERE population >= 200000000
+
+-- 5.Give the name and the per capita GDP for those countries with a population of at least 200 million.
+
+SELECT name, gdp/population
+FROM world
+WHERE population >= 200000000
+
+-- 6.Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
+
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America'
+
+-- 7.Show the name and population for France, Germany, Italy
+
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
+
+-- 8.Show the countries which have a name that includes the word 'United'
+
+SELECT name
+FROM world
+WHERE name LIKE 'United%'
